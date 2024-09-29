0da4923f6d07f45bd4dcb7cd0d0eb1dc0db084f0
Yahaya <developpeur.yb@gmail.com>
2024-09-29 09:50:16 +0200

    add user information manager controller and service

---
+++ api/voluptuaria-api/src/modules/user/controllers/user-informations.controller.ts
@@ -0,0 +1,9 @@
+import { Controller } from "@nestjs/common"
+
+/**
+ * @brief user information's management controller
+ */
+@Controller("user")
+export class UserInformationsController{
+
+}
---
+++ api/voluptuaria-api/src/modules/user/services/user-informations.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from "@nestjs/common"
+
+/**
+ * @brief user information management service
+ */
+@Injectable()
+export class UserInformationsService{
+
+}
