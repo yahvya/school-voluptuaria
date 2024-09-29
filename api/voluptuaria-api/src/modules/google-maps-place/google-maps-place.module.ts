e786899f579b4a6d174b26854d901396b1c8f2cc
Kilimou <kilimouambrois@gmail.com>
2024-09-29 12:48:21 +0200

    add google maps place / openweathermap modules

---
+++ api/voluptuaria-api/src/modules/google-maps-place/google-maps-place.module.ts
@@ -0,0 +1,10 @@
+import {Module} from "@nestjs/common";
+
+/**
+ * @brief google maps places api module
+ */
+@Module({
+
+})
+export class GoogleMapsPlaceModule{
+}
---
+++ api/voluptuaria-api/src/modules/openwheatermap/openweathermap.module.ts
@@ -0,0 +1,10 @@
+import {Module} from "@nestjs/common";
+
+/**
+ * @brief openweathermap api module
+ */
+@Module({
+
+})
+export class OpenweathermapModule{
+}
