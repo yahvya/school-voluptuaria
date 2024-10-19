86ee060706aa80b347b1dd189d0ef73ba01fe30d
ntsn4 <ntsn44@gmail.com>
2024-10-19 03:33:10 +0200

    Add review widget

---
+++ front-end/voluptuaria_front/lib/components/review.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'rating.dart';
+import 'package:voluptuaria_front/resources/themes/colors.dart';
+
+class Review extends StatelessWidget {
+  final Color backgroundColor;
+  final String profileName;
+  final ImageProvider profileImage;
+  final String commentText;
+  final Color commentTextColor;
+  final double commentTextSize;
+  final double borderRadius;
+  final int rating;
+
+  Review({
+    this.backgroundColor = upperBackgroundColor,
+    this.commentTextColor = upperTextColor,
+    this.commentTextSize = 20.0,
+    this.borderRadius = 20,
+    required this.profileName,
+    required this.profileImage,
+    required this.commentText,
+    required this.rating,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: backgroundColor, // Couleur d'arrière-plan du widget
+        borderRadius: BorderRadius.circular(borderRadius), // Arrondit les coins du widget
+      ),
+      padding: EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              CircleAvatar(
+                backgroundImage: profileImage,
+                radius: 24.0,
+              ),
+              SizedBox(width: 16.0),
+              Text(
+                profileName,
+                style: TextStyle(
+                    color: commentTextColor,
+                    fontSize: commentTextSize,
+                ),
+              ),
+              SizedBox(width: 16.0),
+              Rating(onRatingChanged: (rating) {
+                print(rating);
+              }),
+            ]
+          ),
+          SizedBox(height: 16.0),
+          Text(
+            commentText,
+            style: TextStyle(
+              color: commentTextColor,
+              fontSize: commentTextSize,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- front-end/voluptuaria_front/lib/main.dart
+++ front-end/voluptuaria_front/lib/main.dart
@@ -3,6 +3,9 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'components/button.dart';
 import 'components/search_bar.dart';
 import 'components/weather_info.dart';
+import 'components/rating.dart';
+import 'components/review.dart';
+import 'components/blue_background.dart';
 
 void main() {
   runApp(MyApp());
@@ -33,6 +36,15 @@ class MyApp extends StatelessWidget {
                 inf: 10,
                 inf2: 10,
                 weatherIcon: FontAwesomeIcons.wind,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Review(
+                profileName: 'svel',
+                profileImage: const AssetImage('lib/resources/images/profileImage.png'),
+                commentText: "L'endroit est vraiment bien",
+                rating: 1,
               ),
             ),
             Expanded(
