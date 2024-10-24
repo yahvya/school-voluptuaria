a56472b32e1b5e1e2884d2d4a4652fa76c2cf8eb
ntsn4 <ntsn44@gmail.com>
2024-10-24 17:28:17 +0200

    update openweather api

--- api/voluptuaria-api/src/modules/openwheatermap/openweathermap.module.ts
+++ api/voluptuaria-api/src/modules/openwheatermap/openweathermap.module.ts
@@ -1,20 +1,7 @@
 import { Injectable, Module } from "@nestjs/common";
 import { CoordinatesDatas } from "../google-maps-place/data-contracts/coordinates.datas";
+import {PlaceDatas} from "../google-maps-place/data-contracts/place.datas";
 
 @Module({})
 export class OpenWeatherMapModule {
-    private coordinates: CoordinatesDatas | null = null;
-
-    setCoordinates(coordinates: CoordinatesDatas) {
-        this.coordinates = new CoordinatesDatas();
-        this.coordinates.latitude = coordinates.latitude;
-        this.coordinates.longitude = coordinates.longitude;
-        this.coordinates.fullAddress = coordinates.fullAddress;
-    }
-
-    getCoordinates(): CoordinatesDatas | null {
-        return this.coordinates;
-    }
-
-
 }
