b4a1876f857d15fcf7e6ae53634ab4f9b94264fc
ntsn4 <ntsn44@gmail.com>
2024-11-27 17:52:55 +0100

    Fix error commit

--- front-end/voluptuaria_front/lib/main.dart
+++ front-end/voluptuaria_front/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:voluptuaria_front/pages/home_page.dart';
+import 'components/navigation_bar.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +10,47 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Test',
-      home: HomePage(),
+      home: MainScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 2; // Index initial
+
+  void _onIconTap(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Page ${_selectedIndex}'),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: CustomNavigationBar(
+          backgroundColor: Colors.grey[200]!,
+          borderRadius: 30.0,
+          icons: [
+            Icons.search,
+            Icons.bookmark_border,
+            Icons.home_outlined,
+            Icons.place_outlined,
+            Icons.person_outline,
+          ],
+          onIconTap: _onIconTap,
+          currentIndex: _selectedIndex,
+        ),
+      ),
     );
   }
 }
