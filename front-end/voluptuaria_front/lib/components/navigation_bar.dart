df3b1b68528b6e605f020eb508e8a7ae3377afc9
ntsn4 <ntsn44@gmail.com>
2024-11-29 08:42:33 +0100

    Fix navigation bar

--- front-end/voluptuaria_front/lib/components/navigation_bar.dart
+++ front-end/voluptuaria_front/lib/components/navigation_bar.dart
@@ -1,19 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:voluptuaria_front/pages/user_wish_list_page.dart';
+import 'package:voluptuaria_front/pages/home_page.dart';
+import 'package:voluptuaria_front/pages/travel_routes_page.dart';
+import 'package:voluptuaria_front/pages/user_profile_page.dart';
 
 class CustomNavigationBar extends StatelessWidget {
   final Color backgroundColor;
   final double borderRadius;
   final List<IconData> icons;
   final Function(int) onIconTap;
-  final int currentIndex; // Nouvel attribut
+  final int currentIndex;
 
   CustomNavigationBar({
     required this.backgroundColor,
     this.borderRadius = 20.0,
     required this.icons,
     required this.onIconTap,
-    required this.currentIndex, // Initialisation du nouvel attribut
+    required this.currentIndex,
   }) : assert(icons.length == 5, 'Must provide exactly 5 icons.');
+
+  void _handleNavigation(BuildContext context, int index) {
+    onIconTap(index);
+    
+    Widget? nextPage;
+    switch (index) {
+      case 0:
+        nextPage = HomePage();
+        break;
+      case 1:
+        nextPage = UserWishListPage();
+        break;
+      case 2:
+        nextPage = HomePage();
+        break;
+      case 3:
+        nextPage = TravelRoutesPage();
+        break;
+      case 4:
+        nextPage = UserProfilePage();
+        break;
+    }
+
+    if (nextPage != null) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => nextPage!),
+      );
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,11 +61,11 @@ class CustomNavigationBar extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: List.generate(icons.length, (index) {
           return GestureDetector(
-            onTap: () => onIconTap(index),
+            onTap: () => _handleNavigation(context, index),
             child: Icon(
               icons[index],
               size: 35,
-              color: index == currentIndex ? Colors.blue : Colors.black, // Changement de couleur si sélectionné
+              color: index == currentIndex ? Colors.blue : Colors.black,
             ),
           );
         }),
