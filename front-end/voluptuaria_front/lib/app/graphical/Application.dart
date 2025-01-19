8267d501db9d4510cfc3e338e375118ed19bf608
Renkas Vitalii <ntsn44@gmail.com>
2025-01-19 16:14:37 +0100

    update routes

--- front-end/voluptuaria_front/lib/app/graphical/Application.dart
+++ front-end/voluptuaria_front/lib/app/graphical/Application.dart
@@ -2,6 +2,9 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:voluptuaria_front/app/configs/application-routes.dart';
 import 'package:go_router/go_router.dart';
+import 'package:voluptuaria_front/pages/forgot_password_page.dart';
+import 'package:voluptuaria_front/pages/new_travel_route_form_page.dart';
+import 'package:voluptuaria_front/pages/travel_route_details_page.dart';
 import 'package:voluptuaria_front/pages/travel_routes_page.dart';
 import 'package:voluptuaria_front/pages/user_wish_list_page.dart';
 import 'package:voluptuaria_front/pages/place_details.dart';
@@ -60,27 +63,21 @@ class Application extends StatelessWidget{
           name: ApplicationRoutes.TRAVEL_ROUTES_PAGE,
           builder: (context,state) => TravelRoutesPage()
       ),
-      /*
       GoRoute(
           path: "/travel-routes/details",
           name: ApplicationRoutes.TRAVEL_ROUTES_PAGE_DETAILS,
-          builder: (context,state) => ()
+          builder: (context,state) => TravelRouteDetailsPage()
       ),
-      */
-      /*
       GoRoute(
           path: "/travel-routes/new",
           name: ApplicationRoutes.TRAVEL_ROUTES_CREATE,
-          builder: (context,state) => ()
+          builder: (context,state) => NewTravelRouteFormPage()
       ),
-      */
-      /*
       GoRoute(
           path: "/travel-routes/new",
           name: ApplicationRoutes.FORGOT_PASSWORD_PAGE,
-          builder: (context,state) => ()
+          builder: (context,state) => ForgotPasswordPage()
       ),
-      */
     ]
   );
 
