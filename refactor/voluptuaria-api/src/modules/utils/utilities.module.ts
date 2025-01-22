dfc103c0f2f09e6cd05ebe6982e76be3359f56af
yahaya <developpeur.yb @gmail.com>
2025-01-22 10:36:34 +0100

    fix module imports

--- refactor/voluptuaria-api/src/modules/utils/utilities.module.ts
+++ refactor/voluptuaria-api/src/modules/utils/utilities.module.ts
@@ -2,14 +2,16 @@ import { Global, Module } from "@nestjs/common"
 import { StringService } from "./services/string.service"
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { RegisteredPlacesEntity } from "../database/entities/registered-places.entity"
+import { PlaceRebuiltService } from "./services/place-rebuilt.service"
+import { GoogleMapsPlaceModule } from "../google-maps-place/google-maps-place.module"
 
 /**
  * Application utilities module
  */
 @Module({
-    imports: [TypeOrmModule.forFeature([RegisteredPlacesEntity])],
-    providers: [StringService],
-    exports: [StringService],
+    imports: [TypeOrmModule.forFeature([RegisteredPlacesEntity]),GoogleMapsPlaceModule],
+    providers: [StringService,PlaceRebuiltService],
+    exports: [StringService,PlaceRebuiltService],
 })
 @Global()
 export class UtilitiesModule {
